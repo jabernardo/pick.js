a2e77372ad13c76eec6750df2d96d433481d66ca
John Aldrich Bernardo <bjohnaldrich@gmail.com>
2017-12-31 15:12:46 +0000

    object.js: typeof checking fns

--- src/object.js
+++ src/object.js
@@ -1,4 +1,232 @@
-/* global pick $ */
+/* global pick $ HTMLElement NodeList */
+
+/**
+ * Check if 2 values are equal
+ * 
+ * @example
+ * 
+ * var k = {"a":1};
+ * pick.is(k, k);
+ * 
+ * @param {object} obj First object
+ * @param {object} other Second object
+ * @return {boolean}
+ * 
+ */
+pick.is = function(obj, other) {
+  return Object.is(obj, other);
+};
+
+/**
+ * Check if var is empty
+ * 
+ * @example
+ * 
+ * pick.isEmpty({}); // true
+ * 
+ * @param {object} obj Object to be check if empty
+ * @return {boolean}
+ * 
+ */
+pick.isEmpty = function(obj) {
+  return Object.keys(obj).length === 0;
+};
+
+/**
+ * Check if var is element
+ * 
+ * @example
+ * 
+ * $.isElement($("*").get());
+ * 
+ * @param {object} obj Object
+ * @return {boolean}
+ * 
+ */
+pick.isElement = function(obj) {
+  return obj instanceof HTMLElement ||
+    obj instanceof NodeList;
+};
+
+/**
+ * Check if var is array
+ * 
+ * @example
+ * 
+ * pick.isArray([]);
+ * 
+ * @param {object} obj Object
+ * @return {boolean}
+ * 
+ */
+pick.isArray = function(obj) {
+  return Array.isArray(obj);
+};
+
+/**
+ * Check if var is object
+ * 
+ * @example
+ * 
+ * pick.isObject({});
+ * 
+ * @param {obj} obj Variable
+ * @return {boolean}
+ * 
+ */
+pick.isObject = function(obj) {
+  return typeof obj === "object";
+};
+
+/**
+ * Check if var is function
+ * 
+ * @example
+ * 
+ * pick.isFunction(function() {});
+ * 
+ * @param {object} obj Variable
+ * @return {boolean}
+ * 
+ */
+pick.isFunction = function(obj) {
+  return typeof obj === "function";
+};
+
+/**
+ * Check if var is string
+ * 
+ * @example
+ * 
+ * pick.isString(123); // false
+ * 
+ * @param {object} obj Variable
+ * @return {boolean}
+ * 
+ */
+pick.isString = function(obj) {
+  return typeof obj === "string";
+};
+
+/**
+ * Check if var is number
+ * 
+ * @example
+ * 
+ * pick.isNumber("123"); // false
+ * 
+ * @param {object} obj Variable
+ * @return {boolean}
+ * 
+ */
+pick.isNumber = function(obj) {
+  return typeof obj === "number";
+};
+
+/**
+ * Check if var is boolean
+ * 
+ * @example
+ * 
+ * pick.isBoolean("true"); // false
+ * 
+ * @param {object} obj Variable
+ * @return {boolean}
+ * 
+ */
+pick.isBoolean = function(obj) {
+  return typeof obj === "boolean";
+};
+
+/**
+ * Check if var is Date
+ * 
+ * @example
+ * 
+ * pick.isDate(new Date()); // true
+ * 
+ * @param {object} obj Variable
+ * @return {boolean}
+ * 
+ */
+pick.isDate = function(obj) {
+  return obj instanceof Date;
+};
+
+/**
+ * Check if var is RegExp
+ * 
+ * @example
+ * 
+ * pick.isRegExp(/unknown/g);
+ * 
+ * @param {object} obj Variable
+ * @return {boolean}
+ * 
+ */
+pick.isRegExp = function(obj) {
+  return obj instanceof RegExp;
+};
+
+/**
+ * Check if var is Error
+ * 
+ * @example
+ * 
+ * pick.isError(new Error("Something went wrong")); // true
+ * 
+ * @param {object} obj Variable
+ * @return {boolean}
+ * 
+ */
+pick.isError = function(obj) {
+  return obj instanceof Error;
+};
+
+/**
+ * Check if var is NaN
+ * 
+ * @example 
+ * 
+ * pick.isNaN(0/0); // true
+ * 
+ * @param {object} obj Variable
+ * @return {boolean}
+ * 
+ */
+pick.isNaN = function(obj) {
+  return isNaN(obj);
+};
+
+/**
+ * Check if var is null
+ * 
+ * @example
+ * 
+ * pick.isNull(null); // true
+ * 
+ * @param {object} obj Variable
+ * @return {boolean}
+ * 
+ */
+pick.isNull = function(obj) {
+  return obj === null;
+};
+
+/**
+ * Check if var is undefined
+ * 
+ * @example
+ * 
+ * pick.isUndefined(window.sample); // true
+ * 
+ * @param {object} obj Variable
+ * @return {boolean}
+ * 
+ */
+pick.isUndefined = function(obj) {
+  return typeof obj === "undefined";
+};
 
 /**
  * Extend Object
